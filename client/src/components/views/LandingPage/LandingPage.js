853622c2b586e448cb8fee2f6c35d77d33803d0c
G1_HONG <manyfaced3@gmail.com>
2020-04-07 20:21:57 +0900

    clean push

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Axios from "axios";
 
-function LandingPage() {
-  return <div>LandingPage</div>;
+function LandingPage(props) {
+  const onClickHandler = () => {
+    Axios.get("/api/user/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to logout");
+      }
+    });
+  };
+  return (
+    <div>
+      LandingPage <button onClick={onClickHandler}>Logout</button>
+    </div>
+  );
 }
 
 export default LandingPage;
